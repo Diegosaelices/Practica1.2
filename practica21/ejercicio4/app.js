3aa985d2ac904460f26ff302683dc879f004bdbb
Diegosaelices <saelicessaelicesdiego@gmail.com>
2023-09-26 17:11:48 +0200

    nueva edicion

--- practica21/ejercicio4/app.js
+++ practica21/ejercicio4/app.js
@@ -4,10 +4,10 @@
     const FECHA_INICIO=prompt('Dime la fecha de inicio');
     const FECHA_FIN=prompt('Dime la fecha en la que acaba');
 
-    console.log("Estimado " +NOMBRE.trim()+","+
+    console.log("Estimado " +NOMBRE.trim()+" ,"+
         "Bienvenido al curso de "+CURSO.trim()+
         " que se celebrará en la localidad de "+LOCALIDAD.trim()+" entre las fechas "+FECHA_INICIO.trim()+
         " y "+FECHA_FIN.trim()+
         "Espero que el curso se desarrolle según sus expectativas. Estaremos encantados de atenderle"+
-        "Atentamente,"+ 
+        "Atentamente, "/n+ 
         "La dirección");
